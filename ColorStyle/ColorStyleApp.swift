73c3f6adf75e52fa97670fec410a379211b26fc2
James Alan Bush <jamesalanbush1972@gmail.com>
2024-05-04 13:34:07 -0400

    Initial Commit

---
+++ ColorStyle/ColorStyleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorStyleApp.swift
+//  ColorStyle
+//
+//  Created by Xcode Developer on 5/4/24.
+//
+
+import SwiftUI
+
+@main
+struct ColorStyleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
